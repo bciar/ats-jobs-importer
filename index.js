3868b9093c8757b165d398c4523291b5a5e135bb
Rakesh Tembhurne <tj2point0@gmail.com>
2018-09-18 19:08:17 +0530

    Initial commit with Crelate

---
+++ index.js
@@ -0,0 +1,12 @@
+const Crelate = require('./crelate');
+require('dotenv').config();
+
+const crelate = new Crelate(process.env.CRELATE_API_KEY, 43200);
+
+crelate.getJobs()
+.then(response => {
+    console.log('response======>', response.data.Results);
+})
+.catch(error => {
+    console.log('error======>', error);
+});
